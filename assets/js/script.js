bc643b1cc1ef5da6641d03af423bd8430d157016
AbbasBSK <baqa4731@gmail.com>
2025-09-07 23:12:12 +0330

    after start formdata video 21

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,39 @@
 const form = document.getElementById("myForm");
-const ageInput = document.getElementById("age");
+console.log(form.elements);
+console.log([...form]);
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault(); // جلوگیری از ارسال واقعی
 
-  // مقایسه مقدارهای مختلف
-  console.log("value:", ageInput.value, typeof ageInput.value);
-  console.log("valueAsNumber:", ageInput.valueAsNumber, typeof ageInput.valueAsNumber);
-});
+const formData = new FormData(form);
+console.log(formData); 
+console.log([...formData]); // تبدیل به آرایه
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// شیء form درواقع یک HTMLFormElement هست که خودش قابل پیمایش (iterable) می‌باشد.
+// یعنی می‌تونی روی اون با دستوراتی مثل for...of یا ... (spread) پیمایش کنی.
+// form.elements → یه مجموعه (HTMLFormControlsCollection) هست، ولی iterable نیست
+// Array, Map, Set, String, NodeList, Form → iterable هستن.
+// const a = {a:1 , b:2}
+// console.log({...a});
+
+// for (let element of form) {
+//   console.log(element); // هر بار یک input یا select برمی‌گردونه
+// }
