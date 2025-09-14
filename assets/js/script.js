449a5c505f2a3454090cd797e288c0e5b4db3f5b
AbbasBSK <baqa4731@gmail.com>
2025-09-14 23:53:31 +0330

    after object metod video 24

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,57 @@
-console.log(Array(5));
-console.log(Array.of(1, 2, '3'));
+const obj = {};
+console.log(obj);
 
-console.log(Array.from([1,2,3], x => x * 2));
-console.log(Array.from({length:3}, (_,i)=> i+1));
+Object.defineProperty(obj, "x", {
+  value: 10,
+  writable: false,   // نمی‌توان تغییر داد
+  enumerable: true,  // در Object.keys دیده می‌شود
+  configurable: true // می‌توان بعداً حذف یا دوباره تعریف کرد
+});
 
-console.log([1,[2,[3,[4]]]].flat(2));
-console.log([1,[2,[3,[4]]]].flat(Infinity));
+obj.x = 12
 
-console.log(['hi','bye'].flatMap(w => w.split('')));
-console.log([1,2,3].flatMap(x => (x%2===0 ? [] : x)));
-
-console.log([10,2,1].sort((a,b)=>a-b));
-console.log(['b','c','a'].sort());
-
-let a = [1,2,3,4];
-a.fill(0,1,3);
-console.log(a);
+console.log(obj);
 
 
-let arr1 = [1,2,3,4,5];
-arr1.copyWithin(0,3);
-console.log(arr1);
 
-let arr2 = [1,2,3,4,5];
-arr2.copyWithin(1,0,2);
-console.log(arr2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const arr = [123456.78, new Date('2020-01-01T00:00:00Z')];
+// console.log(arr.toLocaleString('fa-IR')); //'en-US' - 'ar-EG'
+
+
+
+
+// const a = [typeof(1, 2, 3)];
+// console.log(a.toString()); // "1,2,3"
+
+// const b = ["1", ["2","3"], "4"];
+// console.log(b[0].toString()); // "1,2,3,4"  — آرایهٔ داخلی هم toString می‌شود
+
+// const c = [typeof(1, undefined, null, 4)];
+// console.log(c.toString());
