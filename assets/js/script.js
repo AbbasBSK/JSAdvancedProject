075fdb88d85f2016d5c23ce070d3a8e35f8be479
AbbasBSK <baqa4731@gmail.com>
2025-09-30 21:36:09 +0330

    after functional programing topic video 34

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,23 @@
-// کلاس اصلی حساب بانکی
-class BankAccount {
-  constructor(owner, balance) {
-    this.owner = owner;      // public field
-    this._balance = balance; // private-ish field با convention _
-  }
+// // تابع خالص
+// const add = (a, b) => a + b;
+// console.log(add(2, 3)); // 5
+// console.log(add(2, 3)); // 5 (همیشه یکسان)
 
-  // Getter برای خواندن موجودی (Encapsulation)
-  get balance() {
-    return this._balance;
-  }
-  // متد واریز (Abstraction)
-  deposit(amount) {
-    if (amount > 0) {
-      this._balance += amount;
-      console.log(`${amount} تومان واریز شد. موجودی جدید: ${this._balance}`);
-    } else {
-      console.log("مقدار واریز نامعتبر است.");
-    }
-  }
+// let c = 10;
+// const addWithC = (a, b) => a + b + c;
+// console.log(addWithC(2, 3)); // 15
+// c = 20;
+// console.log(addWithC(2, 3)); // 25 (خروجی تغییر کرد!)
 
-  // متد برداشت (Abstraction + validation)
-  withdraw(amount) {
-    if (amount > 0 && amount <= this._balance) {
-      this._balance -= amount;
-      console.log(`${amount} تومان برداشت شد. موجودی جدید: ${this._balance}`);
-    } else {
-      console.log("برداشت نامعتبر است!");
-    }
-  }
+// const numbers = [1, 2, 3];
+// // تغییرناپذیر
+// const newNumbers = [...numbers, 4];
+// console.log(newNumbers); // [1, 2, 3, 4]
+// console.log(numbers);    // [1, 2, 3] (تغییر نکرده)
 
-  // متد استاتیک: توضیح درباره نوع حساب
-  static accountType() {
-    console.log("این یک حساب بانکی استاندارد است.");
-  }
-}
+const numbers = [1, 2, 3];
 
-class PremiumAccount extends BankAccount {
-  constructor(owner, balance, cashback) {
-    super(owner, balance); // ارث‌بری + constructor
-    this.cashback = cashback;
-  }
+// map یک تابع مرتبه بالاست
+const doubled = numbers.map(x => x * 2);
 
-  // متد override (پیاده‌سازی جدید متد والد)
-  withdraw(amount) {
-    if (amount <= this._balance) {
-      this._balance -= amount;
-      let cb = amount * this.cashback;
-      this._balance += cb; // برگرداندن نقدی
-      console.log(`${amount} برداشت شد و ${cb} تومان کش‌بک دریافت شد. موجودی جدید: ${this._balance}`);
-    } else {
-      console.log("برداشت نامعتبر است!");
-    }
-  }
-}
-
-
-const myAccount = new BankAccount("Ali", 1000);
-console.log(myAccount.balance); // 1000
-myAccount.deposit(500);
-myAccount.withdraw(200);
-BankAccount.accountType();
-
-const myPremiumAccount = new PremiumAccount("Sara", 2000, 0.1);
-myPremiumAccount.withdraw(300);
+console.log(doubled); // [2, 4, 6]
