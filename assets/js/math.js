ddf89cfa4cf72bdb8f4b8aa362ad0d87f33d4f32
AbbasBSK <baqa4731@gmail.com>
2025-10-06 23:04:15 +0330

    after EcmaScriptModuls topic video 37

---
+++ assets/js/math.js
@@ -0,0 +1,11 @@
+export const add = (a, b) => a + b;
+
+export function multiply(a, b) {
+  return a * b;
+}
+
+export default function greet(name) {
+  return `سلام ${name}!`;
+}
+
+export const numbers = [1, 2, 3, 4, 5];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,8 @@
-import { add } from './main.js';
+import hi, { add , multiply, numbers } from './math.js';
+
 console.log(add(2, 3)); // 5
+console.log(multiply(2, 3)); // 6
+
+console.log(hi('علی')); // سلام علی!
+
+console.log(numbers); // [1, 2, 3, 4, 5]
