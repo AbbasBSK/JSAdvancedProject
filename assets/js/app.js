306cfb2dc1df84ed2ed23322696ca89f59842048
AbbasBSK <baqa4731@gmail.com>
2025-10-17 21:51:36 +0330

    after sigleton pattern  and factory pattern  from creational patterns video 41

--- assets/js/app.js
+++ assets/js/app.js
@@ -51,6 +51,90 @@
 
 
 
+// روش سنتی بدون پترن:
+// const configA = { appName: "MyApp" };
+// const configB = { appName: "MyApp" };
+// دو تا آبجکت جدا هستن و اگر یکی تغییر کنه، اون یکی خبردار نمی‌شه.
+
+
+// class Config {
+//   static instance;
+
+//   constructor(appName) {
+//     // اگر قبلاً نمونه ساخته شده، همونو برگردون
+//     if (Config.instance) {
+//       console.log(`نمونه قبلی برگردونده شد: ${Config.instance.appName}`);
+//       return Config.instance;
+//     }
+
+//     // در غیر این صورت مقدار جدید بساز
+//     this.appName = appName;
+//     Config.instance = this;//یه کپی از اولین نمونه ساخته‌شده در خود کلاس نگه می‌داره.
+//     console.log(`نمونه جدید ساخته شد با نام: ${this.appName}`);
+//   }
+// }
+
+// const config1 = new Config("MyApp");
+// console.log(config1);
+
+// const config2 = new Config("NewApp");
+// console.log(config2.appName);     // "MyApp"
+
+
+
+
+
+
+
+
+// const user1 = { role : "admin", canEdit: true, canDelete: true };
+// const user2 = { role : "guest", canEdit: false, canDelete: false };
+
+
+// function userFactory(role) {
+//   if (role === "admin") {
+//     return { role, canEdit: true, canDelete: true };
+//   }
+//   if (role === "user") {
+//     return { role, canEdit: false, canDelete: false };
+//   }
+// }
+
+// const admin = userFactory("admin");
+// const guest = userFactory("user");
+
+// console.log(admin); // { role: 'admin', canEdit: true, canDelete: true }
+// console.log(guest); // { role: 'user', canEdit: false, canDelete: false }
+
+
+
+
+
+
+function VehicleFactory() {}
+
+VehicleFactory.prototype.createVehicle = function(type) {
+  switch (type) {
+    case "car": return { type: "car", wheels: 4 };
+    case "bike": return { type: "bike", wheels: 2 };
+  }
+};
+
+const factory = new VehicleFactory();
+const car = factory.createVehicle("bike");
+
+console.log(car); // 
+
+
+
+
+
+
+
+
+
+
+
 
 
 
