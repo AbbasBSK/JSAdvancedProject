247117b986cc4ae79bbdfe603fbb780fbc99c461
AbbasBSK <baqa4731@gmail.com>
2025-10-27 23:55:45 +0330

    after Strategy pattern before homework

--- assets/js/app.js
+++ assets/js/app.js
@@ -58,61 +58,15 @@
 // console.log(myCar.cost()); // 21500
 
 
-// روش سنتی:
-let data = 10;
-function updateUI() {
-  console.log("UI updated:", data);
-}
-data = 20;
-updateUI();
+// // روش سنتی:
+// let data = 10;
+// function updateUI() {
+//   console.log("UI updated:", data);
+// }
+// data = 20;
+// updateUI();
 
-// روش دیزاین پترنی (Observer):
-class Subject {
-  constructor() {
-    this.observers = [];
-  }
-
-  subscribe(fn) {
-    this.observers.push(fn);
-  }
-
-  notify(data) {
-    this.observers.forEach(fn => fn(data));
-  }
-}
-
-const subject = new Subject();
-
-subject.subscribe(data => console.log("UI updated:", data));
-subject.subscribe(data => console.log("Logger:", data));
-console.log(subject);
-
-
-subject.notify(42);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// // روش دیزاین پترنی (Observer):
 // class Subject {
 //   constructor() {
 //     this.observers = [];
@@ -120,12 +74,11 @@ subject.notify(42);
 
 //   subscribe(fn) {
 //     this.observers.push(fn);
-//     // console.log("مقداردهی پراپرتی");
+//     console.log("لحظه اعمال تغییر");
 //   }
 
 //   notify(data) {
-//     this.observers.forEach(f => f(data));
-//     // console.log("لحظه اعمال تغییر");
+//     this.observers.forEach(fn => fn(data));
 //   }
 // }
 
@@ -133,23 +86,71 @@ subject.notify(42);
 
 // subject.subscribe(data => console.log("UI updated:", data));
 // subject.subscribe(data => console.log("Logger:", data));
+// console.log(subject);
 
-// // console.log(subject.observers);
-
-// subject.notify(5);
+// subject.notify(42);
 // subject.notify(6);
 
 
 
 
-// const strategies = {
-//   gold: price => price * 0.8,
-//   silver: price => price * 0.9,
-//   normal: price => price
-// };
 
-// function calculate(price, type) {
-//   return strategies[type](price);
-// }
 
-// console.log(calculate(100 , "silver"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// روش سنتی:
+function calculatee(price, type) {
+  if (type === "gold") return price * 0.8;
+  else if (type === "silver") return price * 0.9;
+  else return price;
+}
+
+// روش دیزاین پترنی (Strategy):
+const strategies = {
+  gold: price => price * 0.8,
+  silver: price => price * 0.9,
+  normal: price => price
+};
+
+function calculate(price, type) {
+  return strategies[type](price);
+}
+
+console.log(calculate(100 , "silver"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
